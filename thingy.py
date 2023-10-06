5436054540964384ccc6c98fcc99032105f4dcb9
LiterallyASandwich <Zakuman8@aol.com>
2023-10-06 14:44:44 -0500

    AhahahaaaAHHAHAHAHHAH

--- thingy.py
+++ thingy.py
@@ -6,4 +6,5 @@
 #Quite an enigma, do you not see?
 #Yet what is seeing?
 #But figmenting that which is not touched or heard?
-#Though even when touch, tasted, heard, or smelled, we still see? Do we not?
+#Though even when touched, tasted, heard, or smelled, we still see? Do we not?
+#So what are we? Truly?
